de74be3bda3be9269fc127a4fc97cd02312ab1ce
megan.styles <megan.styles@gmail.com>
2018-11-14 13:29:38 +1300

    Got a tweet sending.

---
+++ TestTwitter/Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TestTwitter
+{
+   [TestClass]
+   public class Tests {
+
+      public ITwitterCredentials __Credentials;
+      //needs to be changed each time as twitter refuses two identical tweets in a row.
+      public string __Tweet = "Hi BePure!";
+
+      [TestMethod]
+      public void SendTweet( ) {
+         __Credentials = TwitterAuth.GenerateCredentials();
+         Auth.SetCredentials(__Credentials);
+         var tweet = Tweet.PublishTweet(__Tweet);
+
+         Assert.IsNotNull(tweet.Id);
+         VerifyTweet(tweet.Id);
+      }
+      
+
+      public void VerifyTweet( long tweetId ) {
+         var tweet = Tweet.GetTweet(tweetId);
+         Assert.AreEqual(__Tweet, tweet.Text);
+      }
+   }
+}
---
+++ TestTwitter/TwitterAuth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
+
+namespace TestTwitter
+{
+   public static class TwitterAuth
+   {
+      public static string __ConsumerKey = ConfigurationManager.AppSettings["TwitterConsumerKey"];
+      public static string __ConsumerSecret = ConfigurationManager.AppSettings["TwitterConsumerSecret"];
+      public static string __AccessToken = ConfigurationManager.AppSettings["TwitterAuthorisationToken"];
+      public static string __AccessTokenSecret = ConfigurationManager.AppSettings["TwitterAuthSecret"];
+
+      public static ITwitterCredentials GenerateCredentials() {
+         return new TwitterCredentials(__ConsumerKey, __ConsumerSecret, __AccessToken, __AccessTokenSecret);
+      }
+      
+   }
+}
